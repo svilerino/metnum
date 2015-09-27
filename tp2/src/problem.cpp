8f0519892233c12e5aa5d8180120916a0b5af8bb
Sil Vilerino <svilerino@gmail.com>
2015-09-27 20:47:40 -0300

    hardcode para resolver metodo de la potencia y ver que onda los tests. Despues se mueve a los metodos que corresponden respetando mejor un diseño claro

--- tp2/src/problem.cpp
+++ tp2/src/problem.cpp
@@ -23,8 +23,57 @@ Problem::Problem(const problem_arguments& args) : csr_ptr(), in_deg_ptr(), is_pa
     } else if(args.is_pagerank){
         csr_ptr = read_args_from_stream_pagerank(input_file,args);
         input_file.close();
-        std::cout << "CSR: " << std::endl << *csr_ptr << std::endl;
-        csr_ptr->print_sparse(std::cout);
+        //std::cout << "CSR: " << std::endl << *csr_ptr << std::endl;
+        //csr_ptr->print_sparse(std::cout);
+
+
+// ---------------------------------------------------------------------------------------------------------
+// ----------------------------------- Hardcode ahead ------------------------------------------------------
+
+        std::vector<double> initial_vec(csr_ptr->cols(), 1/(double)csr_ptr->cols());
+        std::vector<double> res;
+        std::string path_reporte_power_method = "power_method_reporte.txt";
+
+        power_method_stop_criteria_t criterio_parada;
+        criterio_parada.criterio = CRT_K_ITERS_DELTA_DIFF;
+        criterio_parada.valor.delta_diff = args.epsilon;
+        criterio_parada.intervalo_iters_reporte = 1;
+
+        std::ofstream reporte_power_method(path_reporte_power_method);
+
+        if (!reporte_power_method.is_open()) {
+            std::cerr << "Imposible escribir en archivo de reporte de power method: " << path_reporte_power_method << std::endl;
+            exit(-1);
+        }
+
+        reporte_power_method.precision(15);
+        reporte_power_method.setf(std::ios::fixed,std::ios::floatfield);
+
+        csr_ptr->power_method(initial_vec, args.c, criterio_parada, res, reporte_power_method);
+
+        reporte_power_method.close(); // Seguro esta abierto, sino hubiera ejecutado el exit(-1) en el check cuando lo abro;
+
+
+        std::string extracted = args.input_file_path.substr(0, args.input_file_path.find_last_of("\\."));
+
+        std::string path_result_stream = extracted + ".out";
+        std::ofstream result_stream(path_result_stream);
+
+        if (!result_stream.is_open()) {
+            std::cerr << "Imposible escribir en archivo de salida: " << path_result_stream << std::endl;
+            exit(-1);
+        }
+
+        for(double val : res)
+        {
+            result_stream << val << std::endl;
+        }
+
+        result_stream.close(); // Seguro esta abierto, sino hubiera ejecutado el exit(-1) en el check cuando lo abro;
+
+
+// ---------------------------------------------------------------------------------------------------------
+
 
     } else { //es in-deg
         in_deg_ptr = read_args_from_stream_indeg(input_file,args);
