04c391b954c4052bf54e5ca82f8a7a357604dbdc
Laski <Laski@users.noreply.github.com>
2015-09-28 05:52:29 -0300

    Generador de fixtures ideales

---
+++ tp2/src/generar_fixtures_ideales.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+
+
+def generar_fecha(cant_equipos, fecha):
+	for equipo in range(1, cant_equipos+1):
+		rival = equipo + fecha
+		if rival > cant_equipos:
+			rival -= cant_equipos
+		print(fecha, equipo, int(equipo < rival), rival, int(rival < equipo))
+
+
+def generar_fixture_equipos(cant_equipos, cant_fechas):
+	cant_partidos = cant_fechas * cant_equipos
+	print(cant_equipos, cant_partidos)
+	for fecha in range(1, cant_fechas+1):
+		generar_fecha(cant_equipos, fecha)
+
+
+
+cant_equipos = int(sys.argv[1])
+cant_fechas = int(sys.argv[2])
+if cant_fechas >= cant_equipos:
+	print ("Debería haber más equipos que fechas")
+	exit(-1)
+generar_fixture_equipos(cant_equipos, cant_fechas)
