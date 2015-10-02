50f29e90a324e9a9ef03a3da01ce41338545d99b
Laski <Laski@users.noreply.github.com>
2015-10-02 02:41:16 -0300

    Exp4 -> Exp5

--- tp2/src/fixtures_ideales.py
+++ tp2/src/fixtures_ideales.py
@@ -13,19 +13,25 @@ def generar_partido(equipo1, equipo2):
     return ganador, 1, perdedor, 0
 
 
+def evaluar_enfrentamientos_posibles(enfrentamientos_restantes, equipos_jugando_hoy):
+    res = []
+    for enfrentamiento in enfrentamientos_restantes:
+        if (not enfrentamiento[0] in equipos_jugando_hoy) and (not enfrentamiento[1] in equipos_jugando_hoy):
+            res.append(enfrentamiento)
+    return res
+
+
 def generar_fecha(cant_equipos, fecha, enfrentamientos_restantes):
     enfrentamientos_fecha = []
     equipos_jugando_hoy = []
-    enfrentamientos_posibles = enfrentamientos_restantes
-    while len(enfrentamientos_posibles) > 0:
-        enfrentamiento_nuevo = random.choice(enfrentamientos_posibles)
+    enfrentamientos_posibles_hoy = evaluar_enfrentamientos_posibles(enfrentamientos_restantes, equipos_jugando_hoy)
+    while len(enfrentamientos_posibles_hoy) > 0:
+        enfrentamiento_nuevo = random.choice(enfrentamientos_posibles_hoy)
         equipos_jugando_hoy.append(enfrentamiento_nuevo[0])
         equipos_jugando_hoy.append(enfrentamiento_nuevo[1])
         enfrentamientos_restantes.remove(enfrentamiento_nuevo)
         enfrentamientos_fecha.append(enfrentamiento_nuevo)
-        enfrentamientos_posibles = [enfrentamiento for enfrentamiento in enfrentamientos_restantes
-                                    if not (enfrentamiento[0] in equipos_jugando_hoy
-                                    or enfrentamiento[1] in equipos_jugando_hoy)]
+        enfrentamientos_posibles_hoy = evaluar_enfrentamientos_posibles(enfrentamientos_restantes, equipos_jugando_hoy)
     return enfrentamientos_fecha
 
 
@@ -53,26 +59,27 @@ def generar_archivos_input(cant_equipos):
     fin = False
     while not fin:
         cant_fechas += 1
-        with open("exp/exp4/fecha%s.txt" % cant_fechas, "w") as f_output:
+        random.seed(1)
+        with open("exp/exp5/fecha%s.txt" % cant_fechas, "w") as f_output:
             fin, resultados_por_fecha = generar_fixture_equipos(cant_equipos, cant_fechas, f_output)
     return resultados_por_fecha
 
 
 def generar_archivos_test(cant_fechas_totales, c):
     for cant_fechas in range(1, cant_fechas_totales+1):
-        with open("exp/exp4/test%s.in" % cant_fechas, "w") as f_output:
-            print("0 %.2f 1 exp/exp4/fecha%s.txt 0.00001" % (c, cant_fechas), file=f_output)
+        with open("exp/exp5/test%s.in" % cant_fechas, "w") as f_output:
+            print("0 %.2f 1 exp/exp5/fecha%s.txt 0.00001" % (c, cant_fechas), file=f_output)
 
 
 def ejecutar_pagerank(cant_fechas_totales):
     for cant_fechas in range(1, cant_fechas_totales+1):
-        call(["../bin/tp2", "exp/exp4/test%s.in" % cant_fechas, "exp/exp4/test%s.out" % cant_fechas])
+        call(["../bin/tp2", "exp/exp5/test%s.in" % cant_fechas, "exp/exp5/test%s.out" % cant_fechas])
 
 
 def parsear_salida_pagerank(cant_fechas_totales):
     resultado_por_cant_fechas = []
     for cant_fechas in range(1, cant_fechas_totales+1):
-        with open("exp/exp4/test%s.out" % cant_fechas) as f_resultado:
+        with open("exp/exp5/test%s.out" % cant_fechas) as f_resultado:
             resultado_por_cant_fechas.append(f_resultado.readlines())
     res = {}
     for cant_fechas, resultado_cant_fechas in enumerate(resultado_por_cant_fechas):
@@ -121,7 +128,7 @@ def diferencia_con_ideal(orden_por_fecha):
 
 
 def imprimir_salida(cant_equipos, c, diferencias):
-    with open("exp/exp4/resultados/%sequipos%.2fc.res" % (cant_equipos, c), "w") as f_output:
+    with open("exp/exp5/resultados/%sequipos%.2fc.res" % (cant_equipos, c), "w") as f_output:
         for cant_fechas, diferencia in diferencias.items():
             print(cant_fechas, diferencia, file=f_output)
 
@@ -133,7 +140,7 @@ def plotear_salida(cant_equipos, c, diferencias_pagerank, diferencias_estandar):
     plt.ylabel("Diferencia con el ranking ideal")
     plt.xlabel("Cantidad de fechas consideradas")
     plt.legend()
-    plt.savefig("exp/exp4/resultados/%sequipos%.2fc.png" % (cant_equipos, c))
+    plt.savefig("exp/exp5/resultados/%sequipos%.2fc.png" % (cant_equipos, c))
 
 
 def main():
@@ -145,7 +152,6 @@ def main():
         c = float(sys.argv[2])
     else:
         c = 0.85
-    random.seed(5)
     
     resultados_por_fecha = generar_archivos_input(cant_equipos)
     cant_fechas_totales = len(resultados_por_fecha)
