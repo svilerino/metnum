5557438cee638ea12575a5284fb58cdfd8e00932
Laski <Laski@users.noreply.github.com>
2015-10-02 04:20:30 -0300

    Experimento 6

---
+++ tp2/src/exp/exp6/parser.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from subprocess import call
+
+def traducir(f_input, f_output):
+    equipos = {}
+    resultados = []
+    for linea in f_input:
+        if '-' in linea and "[-" not in linea and "finals" not in linea:
+            linea = linea.replace("United States", "United_States").replace("Costa Rica", "Costa_Rica").replace("South Korea", "South_Korea").replace("Bosnia-Herzegovina", "Bosnia_Herzegovina").replace("Côte d'Ivoire", "Cote_dIvoire")
+            palabras = linea.split(" ")
+            palabras_utiles = [palabra for palabra in palabras
+                                if not any([l in palabra for l in ("(",")",".",":","/")])
+                                and len(palabra) != 0]
+            if "-" in palabras_utiles[3]:
+                del palabras_utiles[3]
+            nombre_jugador_1, resultado, nombre_jugador_2 = palabras_utiles[1:4]
+            jugadores = []
+            for nombre_jugador in (nombre_jugador_1, nombre_jugador_2):
+                if not nombre_jugador in equipos.keys():
+                    if len(equipos) > 0:
+                        equipos[nombre_jugador] = max(equipos.values()) + 1
+                    else:
+                        equipos[nombre_jugador] = 1
+                jugadores.append(equipos[nombre_jugador])
+            jugador_1, jugador_2 = jugadores
+            goles_1, goles_2 = resultado.split("-")
+            if goles_1 == goles_2:
+                continue
+            ganador = jugador_1 if goles_1 > goles_2 else jugador_2
+            perdedor = jugador_2 if goles_1 > goles_2 else jugador_1
+            resultados.append((ganador, max(goles_1, goles_2), perdedor, min(goles_1, goles_2)))
+    print(64, len(resultados), file=f_output)
+    for resultado in resultados:
+        print(1, *resultado, file=f_output)
+    return equipos
+
+
+def parsear_e_imprimir():
+    with open("worldcup-original.txt") as f_input:
+        with open("worldcup.txt", "w") as f_output:
+            return traducir(f_input, f_output)
+
+
+def ejecutar_pagerank():
+    call(["../bin/tp2", "exp/exp6/worldcup.in", "exp/exp6/worldcup.out"])
+
+
+equipos = parsear_e_imprimir()
+ejecutar_pagerank()
